06c6059d37cb3a16c2a5509bf02237f97d895af1
lalinw <i.lalinw@gmail.com>
2017-01-23 12:37:30 -0800

    complete

--- BigGoButton/app/src/main/java/lalinw/washington/edu/biggobutton/ButtonActivity.java
+++ BigGoButton/app/src/main/java/lalinw/washington/edu/biggobutton/ButtonActivity.java
@@ -2,12 +2,28 @@ package lalinw.washington.edu.biggobutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ButtonActivity extends AppCompatActivity {
+
+    private int count;
+    Button buttonPush;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
+
+        count = 1;
+        buttonPush = (Button) findViewById(R.id.buttonPush);
+        buttonPush.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick (View view) {
+                //action
+                buttonPush.setText("You have pushed me " + (count++) + " time(s)!");
+            }
+        });
     }
+
 }
